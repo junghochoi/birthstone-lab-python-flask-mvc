bad7d69b784f0aef7c3eee2b03132a68e21d54bf
junghole <dchoi315@gmail.com>
2020-07-15 12:18:51 -0400

    this is for samira

--- app.py
+++ app.py
@@ -15,3 +15,13 @@ app = Flask(__name__)
 @app.route('/index')
 def index():
     return render_template('index.html')
+
+@app.route('/results', methods=["GET", "POST"])
+def results():
+    if request.method == "POST":
+        print(request.form)
+        month = request.form["month"]
+        return render_template('results.html', month=month)
+    else:
+        return "You have to use a post method"
+
